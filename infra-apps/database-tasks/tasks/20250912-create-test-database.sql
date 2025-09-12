cbd30ecea783ebf399707abcdd10381639fd4714
Owen Young <owen@owenyoung.com>
2025-09-13 02:03:20 +0800

    fix database task

--- infra-apps/database-tasks/tasks/20250912-create-test-database.sql
+++ infra-apps/database-tasks/tasks/20250912-create-test-database.sql
@@ -1,5 +1,5 @@
 -- 创建测试数据库和用户
 CREATE DATABASE test_demo_production;
-CREATE USER test_demo_user WITH PASSWORD :'TEST_DEMO_PASSWORD';
+CREATE USER test_demo_user WITH PASSWORD '$TEST_DEMO_PASSWORD';
 GRANT ALL PRIVILEGES ON DATABASE test_demo_production TO test_demo_user;
 
